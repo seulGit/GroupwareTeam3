df21a0d4ea38a5ccce5113d6b2ba8a06c29cccd4
kpolitics <gspst06@gmail.com>
2022-10-04 18:35:41 +0900

    221004 김정치

--- groupware/src/main/java/com/team3/groupware/jeongchi/service/MessageServiceImpl.java
+++ groupware/src/main/java/com/team3/groupware/jeongchi/service/MessageServiceImpl.java
@@ -19,23 +19,42 @@ public class MessageServiceImpl implements MessageService {
 	private MessageDAO messageDao;
 	
 	// 쪽지테이블에 첨부파일이 없는 쪽지 insert 하기
-	@Override
-	public void write(MessageVO messageVo) {
-		// TODO Auto-generated method stub
-		
-		messageDao.write(messageVo);
-	}
-
-	@Override
-	public List<EmployeeVO> writeAddressSearch(String search_option, String keyword) {
-		// TODO Auto-generated method stub
-		return messageDao.writeAddressSearch(search_option, keyword);
-	}
+//	@Override
+//	public void write(MessageVO messageVo) {
+//		// TODO Auto-generated method stub
+//		
+//		messageDao.write(messageVo);
+//	}
 
 	@Override
 	public List<EmployeeVO> writeAddressSearch(Map<String, Object> addSearchMap, String search_option, String keyword) {
 		// TODO Auto-generated method stub
 		return messageDao.writeAddressSearch(addSearchMap, search_option, keyword);
 	}
+
+	@Override
+	public int sendMessage(MessageVO messageVO) {
+		// TODO Auto-generated method stub
+		return messageDao.sendMessage(messageVO);
+	}
+
+	@Override
+	public List<MessageVO> sendMessageList(int start, int end, MessageVO messageVO, int emp_num) {
+		// TODO Auto-generated method stub
+		return messageDao.sendMessageList(start, end, messageVO, emp_num);
+	}
+
+	@Override
+	public int count(MessageVO messageVO) {
+		// TODO Auto-generated method stub
+		return messageDao.count(messageVO);
+	}
+
+	@Override
+	public List<MessageVO> receiveMessageList(int start, int end, MessageVO messageVO, int emp_num) {
+		// TODO Auto-generated method stub
+		return messageDao.receiveMessageList(start, end, messageVO, emp_num);
+	}
+	
 
 }
